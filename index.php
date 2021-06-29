5f56fd8b6619afb91f770cadc2404a51d411c829
rickbruys <70892780+rickbruys@users.noreply.github.com>
2021-06-29 10:52:25 +0200

    files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>GameFest</title>
+</head>
+<body>
+    <div class="index_background">
+        <div class="header_top">
+            <?php require_once 'php/header.php'?>
+        </div>
+        <div class="header_bottom">
+            <p>De beste LAN-parties</p>
+            <a href="#">Meld je aan</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,11 @@
+<div class="header">
+    <div class="logo">
+        <h1>GameFest</h1>
+    </div>
+    <div class="nav">
+        <a href="#">Aanmelden</a>
+        <a href="#">Betalen</a>
+        <a href="#"><img src="../img/white_plus.png" alt="plus"></a>
+        <a href="#">Overzicht</a>
+    </div>
+</div>
